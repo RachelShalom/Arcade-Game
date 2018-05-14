a69becf59de099c494fad7ba8176ee4bde218fc4
Rachel <rshalom1984@gmail.com>
2018-05-14 21:33:46 +0300

    fix collision

--- js/app.js
+++ js/app.js
@@ -9,8 +9,8 @@ var Enemy = function(x, y, speed, width, height) {
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
-    this.width=width;
-    this.height=height;
+    this.width = width;
+    this.height = height;
     this.speed = speed;
 };
 
@@ -24,60 +24,65 @@ Enemy.prototype.update = function(dt) {
         this.x = 0;
     }
     this.x = this.x + this.speed * dt;
-   
-   
+
+
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y,this.width, this.height);
-    
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y, this.width, this.height);
+
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
-var Player = function(x, y,width,height) {
+var Player = function(x, y, width, height) {
     this.sprite = 'images/char-boy.png';
     this.x = x;
     this.y = y;
-    this.width=width;
-    this.height= height;
+    this.width = width;
+    this.height = height;
 }
 
 //Player update method
+var frameNumber = 0;
 Player.prototype.update = function() {
+        frameNumber++; // this is a helper variable for the collision to count frames
         this.x = this.x;
         this.y = this.y;
-        this.checkCollisions();
+        //check coliition only every 15th frame so we will be able to see the collision of player and enemy happenning 
+        if (frameNumber % 15 === 0) {
+            this.checkCollisions();
+        }
         this.win();
-        
+
 
     }
     //Player render the player
 Player.prototype.render = function() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y, this.width, this.height);
-    
+
     }
     //handle keys
 Player.prototype.handleInput = function(direction) {
     switch (direction) {
         case 'left':
             if (this.x >= 101) { this.x = this.x - 101; }
-            console.log("x: "+this.x+" "+"y: "+this.y);
+            console.log("x: " + this.x + " " + "y: " + this.y);
             break;
         case 'up':
-            if( this.y>=83){this.y = this.y - 83;}
-            console.log("x: "+this.x+" "+"y: "+this.y);
+            if (this.y >= 83) { this.y = this.y - 83; }
+            console.log("x: " + this.x + " " + "y: " + this.y);
             break;
         case 'right':
-            if (this.x<404){this.x = this.x + 101;}
-            console.log("x: "+this.x+" "+"y: "+this.y);
+            if (this.x < 404) { this.x = this.x + 101; }
+            console.log("x: " + this.x + " " + "y: " + this.y);
             break;
         case 'down':
-            if(this.y<415){this.y = this.y + 83;}
-            console.log("x: "+this.x+" "+"y: "+this.y);
+            if (this.y < 415) { this.y = this.y + 83; }
+            console.log("x: " + this.x + " " + "y: " + this.y);
             break;
     }
 
@@ -90,40 +95,40 @@ Player.prototype.reset = function() {
 
 //check if the player and enemy are in the same "square" in the grid
 Player.prototype.checkCollisions = function() {
-    for (var i = 0; i < allEnemies.length; i++) {
-        if (this.x +this.width-10>= allEnemies[i].x &&this.x<=allEnemies[i].x+allEnemies[i].width &&this.y ==allEnemies[i].y+20 ) {
-            console.log("coliisssssssssion");
-            this.reset();
+        for (var i = 0; i < allEnemies.length; i++) {
+            if ((this.x + this.width / 2) >= allEnemies[i].x &&
+                this.x <= allEnemies[i].x + allEnemies[i].width &&
+                this.y == allEnemies[i].y + 20) {
+                console.log("coliisssssssssion");
+                this.reset();
+            }
         }
-
     }
-
-}
-//check if the player has a WIN. A winhappens when the player rich the water (this.y=0)
-// Get the modal
+    //check if the player has a WIN. A winhappens when the player rich the water (this.y=0)
+    // Get the modal
 var modal = document.getElementById('myModal');
-var btn= document.getElementById('myBtn');
+var btn = document.getElementById('myBtn');
 
-Player.prototype.win= function(){
-    var p= this; //saving the "this" as the Player object
-     if( this.y ===0){
+Player.prototype.win = function() {
+    var p = this; //saving the "this" as the Player object
+    if (this.y === 0) {
         modal.style.display = "block";
         btn.onclick = function() {
-            modal.style.display = "none"; 
-           p.reset();
+            modal.style.display = "none";
+            p.reset();
         }
-           
-            console.log("WINNNNNNNNNNNNNNNNN");
+
+        console.log("WINNNNNNNNNNNNNNNNN");
     }
-  
-      
-    }
+
+
+}
 
 
 // Now instantiate your objects with with different size.(with the given img the bugs and player width is 101 and height is 171)
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [new Enemy(0, 63, 170,101,171), new Enemy(101, 146, 250,120,171), new Enemy(202, 229,200,101,171)];
+var allEnemies = [new Enemy(0, 63, 170, 101, 171), new Enemy(101, 146, 250, 120, 171), new Enemy(202, 229, 200, 101, 171)];
 var player = new Player(202, 415, 101, 171);
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
